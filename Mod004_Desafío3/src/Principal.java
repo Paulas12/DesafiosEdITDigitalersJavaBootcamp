ea1c5cbe22ee5c638886911092a9bb94d189ec39
Paula Schuppe <107902047+Paulas12@users.noreply.github.com>
2022-07-12 20:38:22 -0300

    Add files via upload

---
+++ Mod004_Desafío3/src/Principal.java
@@ -0,0 +1,21 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i= 1;
+		int suma = 0;
+		
+		while(i <= 25) {
+			if ((i%2) == 0);{
+				suma = suma + i; 
+			}
+		i++; 
+		}
+		System.out.println("La suma de los numeros pares del 1 al 25 es: " + suma);
+
+
+	}
+
+}
