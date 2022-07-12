04146dd6ec910dd45233250395e8502f2328293b
Paula Schuppe <107902047+Paulas12@users.noreply.github.com>
2022-07-12 20:33:40 -0300

    Add files via upload

---
+++ Mod004_Desafío1/src/Principal.java
@@ -0,0 +1,15 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Ejercicio 1 ");
+		for (int i = 1; i < 10; i++) {
+			System.out.println( i + 1);
+		}
+
+		
+	}
+
+}
